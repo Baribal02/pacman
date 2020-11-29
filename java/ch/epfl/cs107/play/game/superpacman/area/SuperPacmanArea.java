690b2b301a8099f196c6eac1f99bfb71b0c6fca2
DarkyKnight02 <63559240+DarkyKnight02@users.noreply.github.com>
2020-11-29 13:03:26 +0100

    Initial commit

---
+++ java/ch/epfl/cs107/play/game/superpacman/area/SuperPacmanArea.java
@@ -0,0 +1,39 @@
+package ch.epfl.cs107.play.game.superpacman.area;
+
+import ch.epfl.cs107.play.game.areagame.Area;
+import ch.epfl.cs107.play.game.superpacman.area.SuperPacmanBehavior;
+import ch.epfl.cs107.play.io.FileSystem;
+import ch.epfl.cs107.play.window.Window;
+
+public class SuperPacmanArea extends Area {
+    private SuperPacmanBehavior behavior;
+    @Override
+    public boolean begin(Window window, FileSystem fileSystem){
+        if(super.begin(window, fileSystem)){
+            behavior = new SuperPacmanBehavior(window, getTitle());
+            setBehavior(behavior);
+            createArea();
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void update(float delta){
+        super.update(delta);
+    }
+    @Override
+    public void end(){
+
+    }
+    public void createArea() {
+        behavior.registerActors(this);
+    }
+    @Override
+    public String getTitle() {
+        return "Super Pac-Man Area";
+    }
+    @Override
+    public final float getCameraScaleFactor(){
+        return 15.f;
+    }
+}
---
+++ java/ch/epfl/cs107/play/game/superpacman/area/SuperPacmanCell.java
@@ -0,0 +1,45 @@
+package ch.epfl.cs107.play.game.superpacman.area;
+
+import ch.epfl.cs107.play.game.areagame.Cell;
+import ch.epfl.cs107.play.game.areagame.actor.Interactable;
+import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
+import ch.epfl.cs107.play.game.actor.Entity;
+import ch.epfl.cs107.play.game.areagame.Cell;
+import ch.epfl.cs107.play.game.areagame.actor.Interactable;
+import ch.epfl.cs107.play.game.areagame.handler.AreaInteractionVisitor;
+
+
+public class SuperPacmanCell extends Cell {
+    private final SuperPacmanBehavior.SuperPacmanCellType type;
+
+    public SuperPacmanCell(int x, int y, SuperPacmanBehavior.SuperPacmanCellType type){
+        super(x, y);
+        this.type = type;
+
+    }
+    @Override
+    public boolean isCellInteractable(){
+        return true;
+    }
+    @Override
+    public boolean isViewInteractable(){
+        return false;
+    }
+    @Override
+    public void acceptInteraction(AreaInteractionVisitor v){
+
+    }
+
+    @Override
+    protected boolean canLeave(Interactable entity) {
+        return true;
+    }
+
+    @Override
+    protected boolean canEnter(Interactable entity) {
+        if ( true){
+            return true;
+        }
+        return false;
+    }
+}
