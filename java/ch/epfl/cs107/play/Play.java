20fa1171c281f927b76a8d5d95e3cc067f27cf45
DarkyKnight02 <63559240+DarkyKnight02@users.noreply.github.com>
2020-12-03 19:24:52 +0100

    Second commit

--- java/ch/epfl/cs107/play/Play.java
+++ java/ch/epfl/cs107/play/Play.java
@@ -1,6 +1,6 @@
 package ch.epfl.cs107.play;
 
-import ch.epfl.cs107.play.game.tutos.Tuto2;
+import ch.epfl.cs107.play.game.superpacman.SuperPacman;
 import ch.epfl.cs107.play.game.Game;
 import ch.epfl.cs107.play.game.areagame.io.ResourcePath;
 import ch.epfl.cs107.play.io.DefaultFileSystem;
@@ -31,8 +31,7 @@ public class Play {
         // Create a demo game :
 		// (it is expected that at the beginning, the provided file does not compile)
        
-        //final Game game = new SuperPacman();
-		Game game = new Tuto2();
+        final Game game = new SuperPacman();
 		// Use Swing display
 		final Window window = new SwingWindow(game.getTitle(), fileSystem, 550, 550);
 		window.registerFonts(ResourcePath.FONTS);
